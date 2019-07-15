066ddcf367619375f0f037a2153e380e3c5b24b6
SOUP <soupyong@gmail.com>
2019-07-15 22:35:11 +0900

    Adjust elevation overlays codes slightly

--- start/src/main/java/soup/codelab/darktheme/MaterialViewBindingAdapter.kt
+++ start/src/main/java/soup/codelab/darktheme/MaterialViewBindingAdapter.kt
@@ -5,8 +5,8 @@ import android.view.View
 import androidx.databinding.BindingAdapter
 import com.google.android.material.shape.MaterialShapeDrawable
 
-@BindingAdapter("useMaterialBackground")
-fun setMaterialBackground(view: View, enabled: Boolean) {
+@BindingAdapter("elevationOverlaysEnabled")
+fun setElevationOverlaysEnabled(view: View, enabled: Boolean) {
     view.background = if (enabled) {
         MaterialShapeDrawable.createWithElevationOverlay(view.context, view.elevation)
     } else {
